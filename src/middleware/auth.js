6fad35ecddf9f047fcac3ef130593badf2f279f6
rohit-kh <rohitkhatri.in@gmail.com>
2021-05-11 20:44:38 +0530

    Fixing auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,6 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-
 const auth = async (req, res, next) => {
     try{
         const token = req.cookies.jwtToken
--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -1,18 +1,17 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 const checkAuth = async (req, res, next) => {
     try{
-        if(typeof req.cookies.jwtToken === 'undefined'){
-            return next()
-        }
         const token = req.cookies.jwtToken
-
         const decoded = jwt.verify(token, process.env.JWT_KEY)
         const user = await User.findOne({_id: decoded._id, 'tokens.token': token })
         if(user){
-            res.redirect('/rooms/select');
+            return res.redirect('/chat');
         }
         next()
     }catch (e){
-        res.status(401).send({error: 'Unauthorized Access'})
+        next()
     }
 }
 
